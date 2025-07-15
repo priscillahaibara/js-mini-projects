e6ce6cf800e1ef21d23bfb8d0478b6aca7518096
Priscilla Haibara <megumi.ufrn@gmail.com>
2025-07-15 19:53:35 -0300

    Add counter logic and rename "count" class to "counter-display" for clarity

---
+++ basics/counter/script.js
@@ -0,0 +1,29 @@
+const counterDisplay = document.querySelector('.counter-display');
+const increase = document.querySelector('.increase');
+const reset = document.querySelector('.reset');
+const decrease = document.querySelector('.decrease');
+
+let count = 0;
+
+function updateDisplay() {
+  counterDisplay.textContent = count;
+}
+
+increase.addEventListener('click', () => {
+  count++;
+  updateDisplay();
+});
+
+decrease.addEventListener('click', () => {
+  if (count > 0) {
+    count--;
+    updateDisplay();
+  }
+});
+
+reset.addEventListener('click', () => {
+  count = 0;
+  updateDisplay();
+})
+
+updateDisplay();
