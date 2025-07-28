c7979b5687adc7964e06a64cc09e77b1e18c7b62
Priscilla Haibara <megumi.ufrn@gmail.com>
2025-07-28 20:30:22 -0300

    Add logic for delete button and task completion

--- basics/todo/script.js
+++ basics/todo/script.js
@@ -10,17 +10,36 @@ addButton.addEventListener('click', function () {
   if (task !== '') {
     const div = document.createElement('div');
     const li = document.createElement('li');
+    const checkbox = document.createElement('input');
     const deleteButton = document.createElement('button');
 
     div.classList.add('div-style');
-    deleteButton.classList.add('delete-button-style')
+    deleteButton.classList.add('delete-button-style');
+    checkbox.type = 'checkbox';
 
     li.textContent = task;
     deleteButton.textContent = 'Delete task';
     input.value = '';
 
-    list.appendChild(div)
+    list.appendChild(div);
+    div.appendChild(checkbox);
     div.appendChild(li);
     div.appendChild(deleteButton);
+
+    /* Adds a listener to mark as completed */
+    checkbox.addEventListener('change', function() {
+      if (checkbox.checked) {
+        li.style.textDecoration = 'line-through';
+      } else {
+        li.style.textDecoration = 'none';
+      }
+    })
+
+    /* Listener to delete the task */
+    deleteButton.addEventListener('click', function() {
+      list.removeChild(div);
+    })
   }
 });
+
+
