5fa920042ce6522f688433b057d4ebc2e04cae8a
Priscilla Haibara <megumi.ufrn@gmail.com>
2025-08-15 12:06:50 -0300

    feat(dog-generator): add loading state when fetching dog image and improve styling for get dog button

--- src/asynchronous/dog-generator/script.js
+++ src/asynchronous/dog-generator/script.js
@@ -4,11 +4,13 @@ const getDogButton = document.querySelector(".getDogButton");
 
 function getNewDog() {
     dogImg.src = '';
+    loadingText.textContent = 'Loading...'
 
     fetch('https://dog.ceo/api/breeds/image/random')
     .then(response => response.json())
     .then(data => {
         console.log(data)
+        loadingText.textContent = ''
         dogImg.src = data.message;
     })
 }
