32f1f325d82270e666c13c83b00cf5091d874576
Priscilla Haibara <megumi.ufrn@gmail.com>
2025-08-15 12:14:25 -0300

    feat(dog-generator): add error handling

--- src/asynchronous/dog-generator/script.js
+++ src/asynchronous/dog-generator/script.js
@@ -3,16 +3,23 @@ const loadingText = document.querySelector(".loadingText");
 const getDogButton = document.querySelector(".getDogButton");
 
 function getNewDog() {
-    dogImg.src = '';
-    loadingText.textContent = 'Loading...'
+  dogImg.src = "";
+  loadingText.textContent = "Loading...";
 
-    fetch('https://dog.ceo/api/breeds/image/random')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        loadingText.textContent = ''
-        dogImg.src = data.message;
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`)
+        } return response.json();
+    })
+    .then((data) => {
+      loadingText.textContent = "";
+      dogImg.src = data.message;
+    })
+    .catch((error) => {
+        console.error(error);
+        loadingText.textContent = 'Failed to load dog'
     })
 }
 
-getDogButton.addEventListener('click', getNewDog)
+getDogButton.addEventListener("click", getNewDog);
