d5dd02a04064ab5d719fff37ab8b74650805716e
Priscilla Haibara <megumi.ufrn@gmail.com>
2025-09-17 20:15:53 -0300

    feat(movie-search): add AbortController to cancel previous fetch requests in movie search

--- src/asynchronous/movie-search/js/api.js
+++ src/asynchronous/movie-search/js/api.js
@@ -9,10 +9,18 @@ import {
 const BASE_URL = "https://www.omdbapi.com/";
 const OMDB_API_KEY = MY_API_KEY;
 
+let controller = null;
+
 async function fetchMovie(movieTitle) {
+  if (controller) controller.abort();
+
+  controller = new AbortController();
+  const signal = controller.signal;
+
   try {
     const response = await fetch(
-      `${BASE_URL}?apikey=${OMDB_API_KEY}&s=${movieTitle}`
+      `${BASE_URL}?apikey=${OMDB_API_KEY}&s=${movieTitle}`,
+      { signal }
     );
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
@@ -27,9 +35,13 @@ async function fetchMovie(movieTitle) {
       renderMessage("No movies found for this title.");
     }
   } catch (err) {
-    console.error("Error fetching movie:", err);
-    renderMessage('Something went wrong. Please try again.')
-    return null;
+    if (err.name === "AbortError") {
+      console.log("Fetch aborted due to new input");
+    } else {
+      console.error("Error fetching movie:", err);
+      renderMessage("Something went wrong. Please try again.");
+      return null;
+    }
   } finally {
     removeSpinner();
   }
